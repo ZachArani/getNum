4bdfcdaeed70823c36f2b08e33355eac08c0fced
yasharAhari <yasharahari@ou.edu>
2019-08-28 11:17:06 -0500

    added comments and fixed spacing

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from collections import deque
 
 
 class S(BaseHTTPRequestHandler):
+    """This class defines what the server do when receives a GET request"""
 
     queue = deque()
     randGen = RandomGen()
@@ -14,16 +15,16 @@ class S(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-
     def do_GET(self):
+        """Defines what the get response is. It is the random number that generated"""
+        # TODO: create the user management if necessary. 
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
         self.wfile.write("{}".format(self.randGen.get_num()).encode('utf-8'))
 
 
-
-
 def run(server_class=HTTPServer, handler_class=S, port=80):
+    """Starts the server using Handler 'S' defined above"""
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
@@ -35,6 +36,7 @@ def run(server_class=HTTPServer, handler_class=S, port=80):
         pass
     httpd.server_close()
     logging.info('Stopping httpd...\n')
+
 
 if __name__ == '__main__':
     from sys import argv
