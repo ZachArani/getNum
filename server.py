86adae1c082f09886cb5816d3c11ddca8918c18d
yasharAhari <yasharahari@ou.edu>
2019-08-28 10:13:23 -0500

    changed so it uses the RandonGen class to get a random number

--- server.py
+++ server.py
@@ -1,22 +1,33 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
-from random import uniform
+from RandomNumGenerator import RandomGen
+from collections import deque
+
+
 class S(BaseHTTPRequestHandler):
+
+    queue = deque()
+    randGen = RandomGen()
+
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
-        #self.wfile.write(round(uniform(1,1000000)).to_bytes(3,byteorder='big'))
-        self.wfile.write("{}".format(round(uniform(1,1000000))).encode('utf-8'))
+        self.wfile.write("{}".format(self.randGen.get_num()).encode('utf-8'))
+
+
+
 
 def run(server_class=HTTPServer, handler_class=S, port=80):
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
+    httpd.timeout = 20
     logging.info('Starting httpd...\n')
     try:
         httpd.serve_forever()
